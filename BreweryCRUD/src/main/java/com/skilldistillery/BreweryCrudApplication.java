ce006f66d928632e149d818d93f2dadf8a274665
BrandenBent <bent_branden@hotmail.com>
2019-09-09 09:56:12 -0600

    l

--- BreweryCRUD/src/main/java/com/skilldistillery/BreweryCrudApplication.java
+++ BreweryCRUD/src/main/java/com/skilldistillery/BreweryCrudApplication.java
@@ -3,11 +3,19 @@ package com.skilldistillery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EntityScan("com.skilldistillery.brewery.entities")
-public class BreweryCrudApplication {
-
+public class BreweryCrudApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	  return application.sources(BreweryCrudApplication.class);
+	}
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BreweryCrudApplication.class, args);
 	}
--- BreweryCRUD/src/main/java/com/skilldistillery/brewery/controllers/BreweriesController.java
+++ BreweryCRUD/src/main/java/com/skilldistillery/brewery/controllers/BreweriesController.java
@@ -9,9 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.skilldistillery.brewery.data.BreweriesDAO;
 import com.skilldistillery.brewery.entities.Breweries;
@@ -23,16 +21,6 @@ public class BreweriesController {
 	@Autowired
 	private BreweriesDAO dao;
 	
-//	@RequestMapping(path = "/", method = RequestMethod.GET)
-//	public String index(Model model) {
-//		model.addAttribute("duck", new RubberDuck());
-//		List<RubberDuck> rubberDucks = dao.findAll();
-//		model.addAttribute("ducks", rubberDucks);
-//
-////	  return "WEB-INF/index.jsp";
-//		return "index";
-//
-//	}
 	
 	@RequestMapping(value={"", "/", "index.do"}, method=RequestMethod.GET)
 	public String index(Model model) {
